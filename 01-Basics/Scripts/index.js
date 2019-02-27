f0f30dd2563601404b099491a037a7668dbe97b6
spsfaculty <kaurrevature@outlook.com>
2019-02-27 17:12:38 -0500

    JavaScript datatypes, truthy and falsy

--- 01-Basics/Scripts/index.js
+++ 01-Basics/Scripts/index.js
@@ -1,1 +1,22 @@
-alert('Hello World');
+//comments
+/*dataTypes:
+number, string, boolean, object, undefined, NaN
+*/
+//Variables
+/*function func1(){
+  alert("x"+x);
+}
+var y=func1();
+alert("y "+y);*/
+var x;
+//alert(typeof(x));
+//x='$'; //global variables
+y=5.00;
+z="Hello World!!"
+// alert(x+y+z);
+// alert(z+(x+y));
+//alert(typeof(λ));//Infinity/0))//x));//10/'abc');
+//truthy and falsy
+//falsy= false,0, undefined, NaN, null, ""
+//alert(0 =="");
+
