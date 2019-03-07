f750a4bca29851ccc20767cfe08834e482835225
Pushpinder Kaur <kaurrevature@outlook.com>
2019-03-07 16:43:05 -0500

    DMLandDDL

---
+++ 02-Week2-SQL/01-Sql Basics/DDLAndDML.sql
@@ -0,0 +1,17 @@
+--DDL:  use to create or manipulate DB objects and delete as well
+-- Create (Database,table,views, Stored Procedure, functions) , Alter (DB, table, SP etc...), 
+-- Drop (DB, table, SP etc..)
+/*create database Music; 
+drop database Music;
+--drop database restaurantDb
+
+if db_id(N'Music') Is not null
+drop database Music;
+go
+create database Music;
+go
+
+alter database Music
+modify name=MusicDb
+*/
+
