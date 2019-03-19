b29fae0af4853a4d7bb4f83cba2c63ba57f84b9e
genesisb17 <genesis.bonds@gmail.com>
2019-03-19 10:58:31 -0400

    initial push from genesis branch -- reviewing core java concepts

---
+++ Java-Review/core-java/src/main/java/com/revature/core/VariableScopes.java
@@ -0,0 +1,23 @@
+package com.revature.core;
+
+public class VariableScopes {
+	
+	/*
+	 * the scope of a variable is simply 
+	 * where the variable is accessible 
+	 * within a class
+	 * 
+	 * instance/object - accessible to all INSTANCE METHODS
+	 *  cannot access instance scoped entities from non 
+	 *  instance scoped entities without an instance
+	 * static/class - accessible everywhere within a class. 
+	 * 	also able to be accessed from the class name itself 
+	 *  without an instance from other classes
+	 * method scope - declared within method OR passed in 
+	 * 	as parameter to method
+	 * block - within curly braces(loops, if statements, 
+	 * 	static blocks, etc)
+	 */
+	
+
+}
