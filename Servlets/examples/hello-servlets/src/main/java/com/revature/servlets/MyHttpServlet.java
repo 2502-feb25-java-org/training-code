c41488fdd7b58f552e97bb194cf1bebd50137f2e
genesisb17 <genesis.bonds@gmail.com>
2019-03-20 16:24:26 -0400

    exploring http servlets

--- Servlets/examples/hello-servlets/src/main/java/com/revature/servlets/MyHttpServlet.java
+++ Servlets/examples/hello-servlets/src/main/java/com/revature/servlets/MyHttpServlet.java
@@ -1,10 +1,33 @@
 package com.revature.servlets;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 
 //make accessible at endpoint http
 public class MyHttpServlet extends HttpServlet{
 	
+	private static Logger log = Logger.getLogger(MyHttpServlet.class);
 	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		log.info("in Http Servlet --GET");
+		PrintWriter writer = resp.getWriter();
+		writer.write("GET METHOD RESPONSE");
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
+	}
 
 }
