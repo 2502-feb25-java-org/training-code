10b03c2a5e701a98309ca3de0e47241bd5a9e956
genesisb17 <genesis.bonds@gmail.com>
2019-03-20 16:48:23 -0400

    understanding basic servlet functionality

--- Servlets/examples/hello-servlets/src/main/java/com/revature/servlets/MyHttpServlet.java
+++ Servlets/examples/hello-servlets/src/main/java/com/revature/servlets/MyHttpServlet.java
@@ -10,10 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
+import com.revature.pojos.User;
+import com.revature.service.UserService;
+
 //make accessible at endpoint http
 public class MyHttpServlet extends HttpServlet{
 	
 	private static Logger log = Logger.getLogger(MyHttpServlet.class);
+	static UserService service = new UserService();	
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
@@ -28,6 +32,31 @@ public class MyHttpServlet extends HttpServlet{
 			throws ServletException, IOException {
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
+		
+		User user = service.getByUsername(username);
+		
+		PrintWriter writer = resp.getWriter();
+		resp.setContentType("text/html");
+		
+		String output = "";
+		
+		if(user == null) {
+			//no user exists 
+			output = "<b>Incorrect user credentials. please go back!</b>";
+		}
+		else {
+			//user exists. must check pw
+			if(password.equals(user.getPassword())){
+				//user has logged in successfully. HERE IS WHERE WE WOULD ADD USER TO SESSION
+				output = "<h1>Welcome, " + user.getUsername() + 
+						"!</h1> <br> " + user.getBio();
+			}
+			else {
+				//unsuccessful log in with wrong pw 
+				output = "<b>Incorrect user credentials. please go back!</b>";
+			}
+		}
+		writer.write(output);
 	}
 
 }
