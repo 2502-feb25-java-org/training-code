5433629e3e61562b4ac1566ee18ba94061fdaf2f
genesisb17 <genesis.bonds@gmail.com>
2019-03-20 14:44:53 -0400

    servlet configurations

--- Servlets/examples/hello-servlets/src/main/java/com/revature/servlets/HelloServlet.java
+++ Servlets/examples/hello-servlets/src/main/java/com/revature/servlets/HelloServlet.java
@@ -8,17 +8,45 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-public class HelloServlet extends GenericServlet{
+import org.apache.log4j.Logger;
 
+public class HelloServlet extends GenericServlet{
+	
+	/*
+	 * Log4j - a logging tool for Java
+	 * - use this instead of basic sysouts 
+	 */
+	private static Logger log =  Logger.getLogger(HelloServlet.class);
+	//LIFECYCLE STEP 1 - INIT
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		log.trace("INITIALIZING"
+				+ " GENERIC SERVLET");
+	}
+
+	
+	//STEP 2 - SERVICE
 	@Override
 	public void service(ServletRequest req, ServletResponse res)
 			throws ServletException, IOException {
-		System.out.println("IN HELLO SERVLET (GENERIC SERVLET) SERVICE METHOD!");
+		log.trace("IN HELLO SERVLET (GENERIC SERVLET) SERVICE METHOD!");
 		
 		PrintWriter writer = res.getWriter();
+		
+		//get servlet params and add to our response
+		String context = getServletContext()
+				.getInitParameter("environmentVar");
 		
 		writer.write("Hello Servlet World!");
 		
 	}
+	//STEP 3 - DESTROY
+	@Override
+	public void destroy() {
+		super.destroy();
+		log.trace("DESTROYING GENERIC SERVLET");
+	}
+
 
 }
