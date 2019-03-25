dd0029b71417f9d6f224011dd34f778e30eda972
genesisb17 <genesis.bonds@gmail.com>
2019-03-25 14:47:07 -0400

    directives

--- angular/hello-angular/src/app/components/directives/directives.component.ts
+++ angular/hello-angular/src/app/components/directives/directives.component.ts
@@ -7,7 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivesComponent implements OnInit {
   items = ['wine', 'steak', 'travel', 'other things that are expensive'];
-
+  employees = [
+    { 
+      name: 'Genesis', 
+      role: 'Trainer'
+    },
+    { 
+      name: 'Pushpinder', 
+      role: 'Trainer'
+    },
+    { 
+      name: 'Patrick', 
+      role: 'Trainer'
+    },
+    { 
+      name: 'David', 
+      role: 'Associate'
+    },
+    { 
+      name: 'Alex', 
+      role: 'Associate'
+    },
+    { 
+      name: 'Tiffany', 
+      role: 'Associate'
+    },
+    
+  ];
   constructor() { }
 
   ngOnInit() {
