dbfead3a5cf9ed48ae79ce4a8361819e9c880005
genesisb17 <genesis.bonds@gmail.com>
2019-04-05 14:40:56 -0400

    more mvc

--- Spring/examples/full-spring/src/main/java/com/revature/controller/TypeController.java
+++ Spring/examples/full-spring/src/main/java/com/revature/controller/TypeController.java
@@ -1,5 +1,38 @@
 package com.revature.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.data.TypeRepository;
+import com.revature.model.Type;
+
+@RestController
+@RequestMapping("/type")
 public class TypeController {
 
+	@Autowired
+	TypeRepository tRepo;
+	
+	@RequestMapping(method=RequestMethod.POST,
+			consumes=MediaType.APPLICATION_JSON_VALUE,
+			produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Type> add(@RequestBody Type t){
+		return new ResponseEntity<Type>(tRepo.save(t), HttpStatus.CREATED);
+	}
+	
+	@RequestMapping(produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Type>> getAll(){
+		return new ResponseEntity<List<Type>>(tRepo.findAll(), HttpStatus.OK);
+	}
+	
+	
+	
 }
--- Spring/examples/full-spring/src/main/java/com/revature/controller/UserController.java
+++ Spring/examples/full-spring/src/main/java/com/revature/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.revature.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,8 +32,25 @@ public class UserController {
 	}
 	
 	//GET ALL USERS
+	@RequestMapping(produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<User>> getAll(){
+		List<User> users = uRepo.findAll();
+		if(users.isEmpty()) {
+			return new ResponseEntity<List<User>>( HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+	}
 	
 	//GET USER BY ID
+	@RequestMapping(value="/{id}")
+	public ResponseEntity<User> getById(@PathVariable int id) {
+		User u = uRepo.findOne(id);
+		if(u==null) {
+			return new ResponseEntity<User>( HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<User>(u, HttpStatus.OK);
+	}
+	
 	
 	//GET USER WITH LOGIN CREDENTIALS
 	
